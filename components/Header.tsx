525a13577c4df3c9c1258f8fdfe3318be2f18362
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-13 11:07:37 +0100

    nav clickable

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Header() {
 	return (
-		<h1 className="text-yellow-400 text-3xl font-bold mt-4 mb-4">Info Site</h1>
+		<>
+			<h1 className="text-yellow-400 text-3xl font-bold mt-4 mb-4">
+				Info Site
+			</h1>
+			<nav className="bg-neutral-800 py-1 px-2 mb-4">
+				<Link href="/" className="mr-3">
+					Welcome
+				</Link>
+				<Link href="/info" className="mr-3">
+					Info
+				</Link>
+				<Link href="/about" className="mr-3">
+					About
+				</Link>
+			</nav>
+		</>
 	);
 }
 
