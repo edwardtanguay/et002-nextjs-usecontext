71496a1ac35e032e2bee1a0440eaaa0b3db4f19c
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-13 10:57:33 +0100

    header in

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
 	return (
-		<h1 className="text-yellow-400 text-3xl font-bold mb-4">Info Site</h1>
+		<h1 className="text-yellow-400 text-3xl font-bold mt-4 mb-4">Info Site</h1>
 	);
 }
 
